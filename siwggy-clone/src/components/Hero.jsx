ebb1944617d0eb98cfab8942fb829b14f054cb85
naveensai <sainaveen9000@gmail.com>
2023-11-01 07:54:43 +0530

    offer-section-

--- siwggy-clone/src/components/Hero.jsx
+++ siwggy-clone/src/components/Hero.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import SectionTitle from './SectionTitle';
 
 import fetchData from '../utils/api';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 import {
   BsFillArrowLeftCircleFill,
@@ -17,11 +19,17 @@ export default function Hero() {
 
   const navigation = (dir) => {
     const container = carouselContainer.current;
-    console.log(container.scrollLeft);
+
+    const rightBtn = document.getElementsByClassName('carouselRighttNav');
+
+    rightBtn.addEventListener('click', () => {
+      container.scrollLeft -= 900;
+    });
+    // console.log(container.scrollLeft);
     const scrollAmount =
       dir === 'left'
-        ? container.scrollLeft - (container.offsetWidth + 40)
-        : container.scrollLeft + (container.offsetWidth + 40);
+        ? container.scrollLeft - (container.offsetWidth + 400)
+        : container.scrollLeft + (container.offsetWidth + 400);
 
     container.scrollTo({
       left: scrollAmount,
@@ -40,6 +48,26 @@ export default function Hero() {
     setLoading(false);
   }, []);
 
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <>
       {loading ? (
@@ -49,7 +77,7 @@ export default function Hero() {
           <div className='flex flex-row items-center justify-between'>
             <SectionTitle text='Best offers for you' />
             {/* button */}
-            <div className='flex items-center gap-x-6 mr-8 text-3xl'>
+            {/* <div className='flex items-center gap-x-6 mr-8 text-3xl'>
               <BsFillArrowLeftCircleFill
                 className='carouselLeftNav arrow'
                 onClick={() => navigation('left')}
@@ -58,45 +86,56 @@ export default function Hero() {
                 className='carouselRighttNav arrow'
                 onClick={() => navigation('right')}
               />
-            </div>
+            </div> */}
           </div>
-          <div className='flex gap-4 ' ref={carouselContainer}>
-            {data &&
-              data.map((item) => {
-                const { accessibility, action, imageId } = item;
+          <div className=' gap-4 ' ref={carouselContainer}>
+            <Carousel
+              autoPlay={true}
+              autoPlaySpeed={1000}
+              keyBoardControl={true}
+              containerClass='carousel-container'
+              responsive={responsive}
+              showDots={true}
+              dotListClass='custom-dot-list-style'
+              itemClass='carousel-item-padding-40-px'
+            >
+              {data &&
+                data.map((item) => {
+                  const { accessibility, action, imageId } = item;
 
-                const { altText } = accessibility;
-                const { link, text } = action;
+                  const { altText } = accessibility;
+                  const { link, text } = action;
 
-                const imgCaro =
-                  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/';
+                  const imgCaro =
+                    'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/';
 
-                return (
-                  <div
-                    className='mt-12 ml-8 grid grid-cols-1 md:grid-cols-2'
-                    key={item.id}
-                  >
-                    <div className=' card card-side max-w-[38rem] bg-base-300 shadow-xl '>
-                      <figure>
-                        <img
-                          src={imgCaro + imageId}
-                          className='h-full object-cover w-[22rem]'
-                          alt='photo-carousel'
-                        />
-                      </figure>
-                      <div className='card-body'>
-                        <h2 className='card-title'>{altText}</h2>
-                        <p>{text}</p>
-                        <div className='card-actions  justify-end mt-4'>
-                          <button className='btn btn-primary uppercase'>
-                            ORDER NOW
-                          </button>
-                        </div>
+                  return (
+                    <div className='mt-12 ml-8 p-6 ' key={item.id}>
+                      <div className=' card card-side max-w-[38rem] bg-base-300 shadow-xl  '>
+                        <figure>
+                          <img
+                            src={imgCaro + imageId}
+                            className='h-full object-cover w-full'
+                            alt='photo-carousel'
+                          />
+                        </figure>
+                        {/* <div className='card-body'>
+                          <h2 className='card-title'>{altText}</h2>
+                          <p>{text}</p>
+                          <div className='card-actions  justify-end mt-4'>
+                            <button
+                              className='btn btn-primary uppercase'
+                              disabled={true}
+                            >
+                              ORDER NOW
+                            </button>
+                          </div>
+                        </div> */}
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+            </Carousel>
           </div>
         </div>
       )}
