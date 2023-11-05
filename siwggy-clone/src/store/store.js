3214f50f786ec8990f2b54cc0919718f81816eea
naveensai <sainaveen9000@gmail.com>
2023-11-05 23:01:13 +0530

    cart-slice-setup

--- siwggy-clone/src/store/store.js
+++ siwggy-clone/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../features/user/userSlice';
+import cartSlice from '../features/cartSlice';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
+    cart: cartSlice,
   },
 });
 export default store;
